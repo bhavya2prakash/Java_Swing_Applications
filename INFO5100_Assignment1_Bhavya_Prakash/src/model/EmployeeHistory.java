14ad559aacbb6840c78d26337b17af4e031b8b3d
bhavya2prakash <prakash.b@northeastern.edu>
2022-09-30 18:15:24 -0400

    Added Create Operation

---
+++ INFO5100_Assignment1_Bhavya_Prakash/src/model/EmployeeHistory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author BHAVYA PRAKASH
+ */
+public class EmployeeHistory {
+    
+    private ArrayList<Employee> employeeList ;
+    
+    public EmployeeHistory(){
+        this.employeeList = new ArrayList<>();
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+    
+    
+}
